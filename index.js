b07641ac2d2fb78bb7c48a9f8dc26a5a461d4a50
lcastienc <2023_luis.castillo@iticbcn.cat>
2023-11-22 19:59:47 +0100

    Inicio pescamines modificación

--- index.js
+++ index.js
@@ -11,30 +11,28 @@ function iniciarPartida() {
     // console.log("hola")
 }
 
-// function crearTaulell(filas,columnas){
-//     let taulell = document.getElementById("taulell");
-//     let tabla = document.createElement("table");
+function setMines(filas, columnas) {
+    let areaTotal = filas * columnas;
+    let minasAIntroducir = Math.floor(areaTotal * 0.17);
 
-//      for (let index = 0; index < filas; index++) {
-//         let fila = document.createElement("tr");
-//         for (let ini = 0; ini < columnas; ini++) {
-//             let celda = document.createElement("td");
-//             let imagen = document.createElement("img");
-//             celda.setAttribute("data-mina","false");
-//             imagen.src="fons20px.jpg";
-//             imagen.onclick = function(){
-//                 obreCasella(`Fila: ${index}, Columna: ${ini}`);
-//             };
-//             celda.appendChild(imagen);
-//             fila.appendChild(celda);
-//         }
-//         tabla.appendChild(fila);
-//      }
-//      taulell.innerHTML="";
-//      taulell.appendChild(tabla);
-//      setMines(filas,columnas);
-// }
+    console.log(minasAIntroducir);
 
+    let minasInGame = 0;
+    while (minasInGame < minasAIntroducir) {
+        let filasAleatorias = Math.floor(Math.random() * filas);
+        let columnasAleatorias = Math.floor(Math.random() * columnas);
+
+        let celda = document.querySelector('#taulell table tr:nth-child(' + (filasAleatorias + 1) + ') td:nth-child(' + (columnasAleatorias + 1) + ')');
+
+        if (celda.getAttribute("data-mina") !== "true") {
+            celda.setAttribute("data-mina", "true");
+            minasInGame++;
+        }
+    }
+    console.log(minasInGame);
+}
+
+//Aportacion para completar de Marco
 function crearTaulell() {
     let taulell = document.getElementById("taulell");
     let tabla = `<table>`;
@@ -51,6 +49,7 @@ function crearTaulell() {
     document.getElementById("taulell").innerHTML = tabla;
     setMines(filas,columnas);
 }
+
 
 function setMines(filas, columnas) {
     let areaTotal = filas * columnas;
